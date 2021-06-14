723ce0ddbe361ff53f8e27d5cc17fdb0983f6c33
MAS00N <66094125+MAS00N@users.noreply.github.com>
2021-06-14 09:09:17 -0500

    Initial Commit

---
+++ GameMonster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameMonster
+//
+//  Created by Mason Dierkes on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameMonster/GameMonsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMonsterApp.swift
+//  GameMonster
+//
+//  Created by Mason Dierkes on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GameMonsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
